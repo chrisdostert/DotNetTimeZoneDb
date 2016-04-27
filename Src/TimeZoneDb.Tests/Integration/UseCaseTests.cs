00f1e8f2e1a98f429dca6b8436a637c99fc6170c
sandrock <antoine.sottiau@gmail.com>
2016-04-27 11:18:20 +0200

    issue #7: use Lazy<> instead of manual code

--- Src/TimeZoneDb.Tests/Integration/UseCaseTests.cs
+++ Src/TimeZoneDb.Tests/Integration/UseCaseTests.cs
@@ -11,24 +11,14 @@ namespace TimeZoneDb.Tests.Integration
     {
         #region Fields
 
-        private static ITimeZoneDbUseCases _timeZoneDbUseCases;
+        private static Lazy<ITimeZoneDbUseCases> _timeZoneDbUseCases = new Lazy<ITimeZoneDbUseCases>(() => new TimeZoneDbUseCases());
 
         #endregion
-
-        [TestInitialize]
-        public void Initialize()
-        {
-            // this became static because instantiating this thing takes ages
-            if (_timeZoneDbUseCases == null)
-            {
-                _timeZoneDbUseCases = new TimeZoneDbUseCases();
-            }
-        }
 
         [TestMethod]
         public void GetAllTimeZones()
         {
-            var allTimeZones = _timeZoneDbUseCases.GetAllTimeZones();
+            var allTimeZones = _timeZoneDbUseCases.Value.GetAllTimeZones();
             // explicit expected count is arbitrary but should be a reasonable number
             Assert.IsTrue(allTimeZones.Count() > 400);
         }
@@ -36,14 +26,14 @@ namespace TimeZoneDb.Tests.Integration
         [TestMethod]
         public void GetTimeZoneWithIanaId()
         {
-            var timeZone = _timeZoneDbUseCases.GetTimeZoneWithIanaId("America/Los_Angeles");
+            var timeZone = _timeZoneDbUseCases.Value.GetTimeZoneWithIanaId("America/Los_Angeles");
             Assert.IsNotNull(timeZone);
         }
 
         [TestMethod]
         public void GetTimeZoneWithIanaId_WrongCaseResolves()
         {
-            var timeZone = _timeZoneDbUseCases.GetTimeZoneWithIanaId("america/los_angeles");
+            var timeZone = _timeZoneDbUseCases.Value.GetTimeZoneWithIanaId("america/los_angeles");
             Assert.IsNotNull(timeZone);
         }
     }
